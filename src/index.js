22ee953fbbcc622dd958e748551094836b1b99ff
lucas <lucasisanderson@gmail.com>
2023-08-06 16:09:47 -0400

    added oauth skeleton, switched source type to modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
-const express = require("express");
+import dotenv from "dotenv";
+dotenv.config({});
+
+import express from "express";
 const app = express();
-// eslint-disable-next-line no-undef
-const PORT = process.env.NODE_ENV || 3500;
+
+import { google } from "googleapis";
+
+const PORT = process.env.PORT || 3500;
+
+const oauth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URL);
+
+const scopes = ["https://www.googleapis.com/auth/calendar"];
+
+app.get("/google", (req, res) => {
+	const url = oauth2Client.generateAuthUrl({
+		// 'online' (default) or 'offline' (gets refresh_token)
+		access_type: "offline",
+		scope: scopes,
+	});
+
+	res.redirect(url);
+});
+
+app.get("/google/redirect", (req, res) => {
+	res.send("active");
+});
+
+app.listen(PORT, () => {
+	console.log("Server started on part", PORT);
+});
