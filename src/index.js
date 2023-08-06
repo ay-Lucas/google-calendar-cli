b64d742fa9e5527514f39ef562310d5d2f25f6f4
lucas <lucasisanderson@gmail.com>
2023-08-06 19:13:57 -0400

    google calendar api now connected, can add hardcoded events, added dayjs dependency

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ dotenv.config({});
 import express from "express";
 const app = express();
 
+import dayjs from "dayjs";
 import { google } from "googleapis";
 import { v4 as uuid } from "uuid";
+const calendar = google.calendar({
+	version: "v3",
+	auth: process.env.GOOGLE_CALENDAR_API_KEY,
+});
 const PORT = process.env.PORT || 3500;
 
 const oauth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URL);
@@ -24,44 +29,34 @@ app.get("/google", (req, res) => {
 
 app.get("/oauth2callback", async (req, res) => {
 	const code = req.query.code;
-	const { tokens } = await oauth2Client.getTokens(code);
+	const { tokens } = await oauth2Client.getToken(code);
 	oauth2Client.setCredentials(tokens);
 	res.send({ msg: "Successful login" });
-	res.send("active");
+	// console.log(oauth2Client.getToken(code));
+	// console.log(code);
+	// res.send("active");
 });
 
 app.get("/schedule_event", async (req, res) => {
-	console.log(oauth2Client.credentials.access_token);
+	// console.log(oauth2Client.credentials.access_token);
 	await calendar.events.insert({
 		calendarId: "primary",
 		auth: oauth2Client,
-		conferenceDataVersion: 1,
 		requestBody: {
 			summary: "test event",
 			description: "pls work, oh plss work",
 			start: {
-				dateTime: new Date().toLocaleString([], {
-					year: "numeric",
-					month: "numeric",
-					day: "numeric",
-				}),
+				dateTime: dayjs(new Date()).add(1, "day").toISOString(),
+				timeZone: "America/New_York",
 			},
 			end: {
-				dateTime: new Date().toLocaleString([], {
-					year: "numeric",
-					month: "numeric",
-					day: "numeric",
-				}),
-			},
-			conferenceData: {
-				createRequest: {
-					requestId: uuid(),
-				},
+				dateTime: dayjs(new Date()).add(2, "day").toISOString(),
+				timeZone: "America/New_York",
 			},
 		},
 	});
+	res.send({ msg: "Done" });
 });
 app.listen(PORT, () => {
 	console.log("Server started on part", PORT);
-	console.log(oauth2Client);
 });
