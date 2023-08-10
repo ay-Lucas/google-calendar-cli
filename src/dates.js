f5f675ae618142ee5c77802b535b3ad5cacbe951
lucas <lucasisanderson@gmail.com>
2023-08-09 22:41:17 -0400

    added shorthand support for 'p' in 'pm' and 'a' in 'am' for adding events.

--- src/dates.js
+++ src/dates.js
@@ -34,27 +34,46 @@ function parseHours(time) {
 function parseMinutes(time) {
 	return time.split(":")[1];
 }
-function validateTime(hour, minutes) {
+function validateTime(time) {
+	let hour, minutes;
 	let str;
+	// split time into hours and minutes
+	// if no minutes are given
+	// console.log(time);
+	if (time.indexOf(":") === -1) {
+		if (time.endsWith("pm") || time.endsWith("p")) {
+			str = time.endsWith("pm") ? "pm" : "p";
+			hour = parseInt(time.split(str)[0]);
+			hour = hour + 12 > 23 ? 0 : hour + 12;
+		} else if (time.endsWith("am") || time.endsWith("a")) {
+			str = time.endsWith("am") ? "am" : "a";
+			hour = parseInt(time.split(str)[0]);
+		}
+		minutes = 0;
+		console.log(hour, minutes);
+		return [hour, minutes];
+	}
+	hour = parseHours(time); // Type === Number
+	minutes = parseMinutes(time); // Type === String
+
 	console.log(hour, minutes);
 	minutes = minutes.toLowerCase();
 	if (minutes.endsWith("pm") || minutes.endsWith("p")) {
 		str = minutes.endsWith("pm") ? "pm" : "p";
-		hour = hour + 12;
+		hour = hour + 12 > 23 ? 0 : hour + 12;
 	} else if (minutes.endsWith("am") || minutes.endsWith("a")) {
 		str = minutes.endsWith("am") ? "am" : "a";
 	}
-	if (hour > 23) {
-		hour = 0;
-	}
+
 	minutes = parseInt(minutes.split(str, 1)[0]);
 	return [hour, minutes];
 }
 export function parseDate(string) {
 	let time, date, month, day, year, hour, minutes, formattedDate;
-	const isTimeStr = (str) => str.indexOf(":") !== -1;
+	// date delimited by '/' or '-'
+	const isTimeStr = (str) => str.indexOf("/") === -1 && str.indexOf("-") === -1;
 
-	if (string.indexOf(" ") === -1 || string.indexOf(":") === -1) return;
+	if (string.indexOf(" ") === -1) return;
 	// split date and time
 	let split = string.split(" ");
 	console.log(split);
@@ -63,21 +82,21 @@ export function parseDate(string) {
 	// checks for empty indicies
 	split = split.filter(isEmpty);
 	console.log(split);
-	if (split.length === 2) {
-		// index that has a colon ':' indicates a time.
-		// reverses array if the first index does not contain a colon ':'
-		let timeIndex = split.findIndex(isTimeStr);
-		console.log(timeIndex);
-		split = timeIndex > 0 ? split.reverse() : split;
+	if (split.length !== 2) {
+		// console.log("");
+		throw new Error("Argument must provide both Date and Time, separated by a space");
 	}
-	time = split[0];
-	hour = parseHours(time); // Type === Number
-	minutes = parseMinutes(time); // Type === String
+	// index that has a colon ':' indicates a time.
+	// reverses array if the first index does not contain a colon ':'
+	let timeIndex = split.findIndex(isTimeStr);
+	console.log(timeIndex);
+	split = timeIndex > 0 ? split.reverse() : split;
+
 	console.log(minutes);
-	[hour, minutes] = validateTime(hour, minutes);
+	time = split[0];
 	date = split[1];
-	// split time into hours and minutes
-	// minutes = time.split(":")[1];
+	[hour, minutes] = validateTime(time);
+
 	// split date into month and day and possibly year
 	date = date.split("/");
 	month = date[0];
@@ -86,15 +105,8 @@ export function parseDate(string) {
 	// add full year if abbreviated
 	year = year.length === 2 ? `20${year}` : year;
 	console.log(year, month, day, hour, minutes);
-	try {
-		// convert String to Number
-		minutes = parseInt(minutes);
-		// hour = parseInt(hour);
-		day = parseInt(day);
-		month = parseInt(month) - 1;
-	} catch (error) {
-		console.log(`Integer parsing error ${error}`);
-	}
+	day = parseInt(day);
+	month = parseInt(month) - 1;
 	let d = new Date();
 	formattedDate = dayjs(d);
 	console.log(year, month, day, hour, minutes);
